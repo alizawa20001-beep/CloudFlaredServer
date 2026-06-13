bc8885c2ba2753f775686b3d67e9367b720e6754
alizawa20001-beep <ali.zawa20001@gmail.com>
2026-06-14 00:26:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -570,8 +570,9 @@ app.post("/api/list-lab-workers", ensureUserId, async (req, res) => {
   }
 });
 
+
 const PORT = 6060;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`\n${"=".repeat(60)}`);
   console.log(`🚀 Worker Management Server Running`);
   console.log(`${"=".repeat(60)}`);
